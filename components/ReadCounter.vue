1e0ebc9baee8c22b0a6fbca5f9f672e6e6f17f75
yuyinws <lyc657508366@gmail.com>
2023-12-10 21:33:58 +0800

    chore: read count

--- components/ReadCounter.vue
+++ components/ReadCounter.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 const { data } = await useAsyncData('counter', () => $fetch('/pv?v=1'))
+
+const counter = ref(0)
+
+watch(data, (val) => {
+  if (val?.state === 'ok' && val?.message) {
+    const start = 0
+    const end = Number(val?.message)
+
+    const duration = 1000 // 滚动的时间
+    const stepTime = Math.abs(Math.floor(duration / (end - start))) // 单步滚动的时间间隔
+
+    counter.value = start
+    const timer = setInterval(() => {
+      counter.value += 1
+      if (counter.value >= end)
+        clearInterval(timer)
+    }, stepTime)
+  }
+})
 </script>
 
 <template>
-  <div flex items-center>
+  <div class="text-gray-500 dark:text-gray-100" flex items-center>
     <i class="i-ri:eye-line" />
-    <span ml-2>{{ data?.message || 0 }}</span>
+    <span ml-2>{{ counter }}</span>
   </div>
 </template>
