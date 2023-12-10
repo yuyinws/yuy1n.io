92d4d3ca9e2c59ca506d167aa3cd48a61c782e2d
yuyinws <lyc657508366@gmail.com>
2023-12-10 21:27:17 +0800

    chore: prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/articles/**': { prerender: true },
     '/': { prerender: true },
+    '/articles': { prerender: true },
   },
   nitro: {
     prerender: {
